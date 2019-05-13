9a2735600899cee8ecf6b4b4d3f268af4ca774de
MISBAH <misbah@MacBook-Pro-1.local>
2019-05-13 15:15:47 +0700

    Initial commit

---
+++ app/src/main/java/com/miss/loginscreen2/MainActivity.java
@@ -0,0 +1,60 @@
+package com.miss.loginscreen2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText1 ,editText2; //Deklarasi object dari class EdiText
+    String text1 ,text2; //Deklarasi object string
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setTitle("User Login Sederhana");
+        getSupportActionBar().setSubtitle("okedroid.com");
+
+    }
+
+    public void loginMasuk(View view) {
+        //Method onClick pada Button
+
+        editText1 = (EditText)findViewById(R.id.edittext_username);
+        editText2 = (EditText) findViewById(R.id.edittext_password);
+        text1 = editText1.getText().toString();
+        text2 = editText2.getText().toString();
+
+        //Kondisi jika username dan password benar maka akan menampilkan pesan text toast
+        //Login sukses dan masuk ke activity 2
+        if ((text1.contains("Username"))&&((text2.contains("Password")))) {
+            Toast.makeText(this, "Login Sukses", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+            startActivity(intent);
+        }
+
+        else if ((text1.matches("")||text2.matches("")))
+          /*
+          Atau jika input text 1 dan text 2 kosong
+           */
+        {
+            //Maka akan menampilkan pesan text toast
+            Toast.makeText(this, "Isikan Username dan Password", Toast.LENGTH_SHORT).show();
+
+        }
+
+        else {
+            //jika kedua kondisi diatas tidak memenuhi
+
+            Toast.makeText(this, "Login Gagal /Username Password Salah", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+
+}
